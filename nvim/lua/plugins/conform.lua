045ec148e563fdaeadbb80c1278835b198f58d87
Francesco Pasqua <cesconix@me.com>
2024-04-13 15:36:08 +0200

    chore: minor changes

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,59 +1,57 @@
 local utils = require 'utils'
 
 return {
-  {
-    'stevearc/conform.nvim',
-    lazy = false,
-    config = function()
-      local conform = require 'conform'
+  'stevearc/conform.nvim',
+  lazy = false,
+  config = function()
+    local conform = require 'conform'
 
-      local formatters_by_ft = {
-        lua = { 'stylua' },
-        markdown = { 'mdformat' },
-        css = { 'prettier' },
-        html = { 'prettier' },
-        yaml = { 'prettier' },
-        json = { 'biome', 'prettier' },
-        javascript = { 'biome', 'prettier' },
-        typescript = { 'biome', 'prettier' },
-        javascriptreact = { 'biome', 'prettier' },
-        typescriptreact = { 'biome', 'prettier' },
-      }
+    local formatters_by_ft = {
+      lua = { 'stylua' },
+      markdown = { 'mdformat' },
+      css = { 'prettier' },
+      html = { 'prettier' },
+      yaml = { 'prettier' },
+      json = { 'biome', 'prettier' },
+      javascript = { 'biome', 'prettier' },
+      typescript = { 'biome', 'prettier' },
+      javascriptreact = { 'biome', 'prettier' },
+      typescriptreact = { 'biome', 'prettier' },
+    }
 
-      local formatters = {
-        biome = {
-          condition = function()
-            return utils.file_exists {
-              'biome.json',
-            }
-          end,
-        },
-        prettier = {
-          condition = function()
-            return utils.file_exists {
-              '.prettierrc.js',
-            }
-          end,
-        },
-      }
-
-      conform.setup {
-        format_on_save = function()
-          return {
-            timeout_ms = 500,
-            lsp_fallback = true,
+    local formatters = {
+      biome = {
+        condition = function()
+          return utils.file_exists {
+            'biome.json',
           }
         end,
-        formatters_by_ft = formatters_by_ft,
-        formatters = formatters,
-      }
+      },
+      prettier = {
+        condition = function()
+          return utils.file_exists {
+            '.prettierrc.js',
+          }
+        end,
+      },
+    }
 
-      vim.keymap.set('n', '<leader>F', function()
-        conform.format {
+    conform.setup {
+      format_on_save = function()
+        return {
+          timeout_ms = 500,
           lsp_fallback = true,
-          async = true,
         }
-      end, { desc = 'Format buffer' })
-    end,
-  },
+      end,
+      formatters_by_ft = formatters_by_ft,
+      formatters = formatters,
+    }
+
+    vim.keymap.set('n', '<leader>F', function()
+      conform.format {
+        lsp_fallback = true,
+        async = true,
+      }
+    end, { desc = 'Format buffer' })
+  end,
 }
--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,65 +1,63 @@
 local utils = require 'utils'
 
 return {
-  {
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local lint = require 'lint'
+  'mfussenegger/nvim-lint',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local lint = require 'lint'
 
-      local linters_by_ft = {
-        markdown = { 'markdownlint' },
-        dockerfile = { 'hadolint' },
-        javascript = { 'biomejs', 'eslint' },
-        typescript = { 'biomejs', 'eslint' },
-        json = { 'biomejs', 'jsonlint' },
-      }
+    local linters_by_ft = {
+      markdown = { 'markdownlint' },
+      dockerfile = { 'hadolint' },
+      json = { 'biomejs', 'jsonlint' },
+      javascript = { 'biomejs', 'eslint' },
+      typescript = { 'biomejs', 'eslint' },
+    }
 
-      local linters = {
-        biomejs = {
-          condition = function()
-            return utils.file_exists {
-              'biome.json',
-            }
-          end,
-        },
-        eslint = {
-          condition = function()
-            return utils.file_exists {
-              '.eslintrc',
-              '.eslintrc.json',
-            }
-          end,
-        },
-      }
+    local linters = {
+      biomejs = {
+        condition = function()
+          return utils.file_exists {
+            'biome.json',
+          }
+        end,
+      },
+      eslint = {
+        condition = function()
+          return utils.file_exists {
+            '.eslintrc',
+            '.eslintrc.json',
+          }
+        end,
+      },
+    }
 
-      lint.linters_by_ft = linters_by_ft
+    lint.linters_by_ft = linters_by_ft
 
-      local function try_lint()
-        local names = lint.linters_by_ft[vim.bo.filetype]
-        local filtered_linters = {}
+    local function try_lint()
+      local names = lint.linters_by_ft[vim.bo.filetype]
+      local filtered_linters = {}
 
-        if names then
-          for _, name in ipairs(names) do
-            local linter = linters[name]
-            if not linter or (linter and linter.condition()) then
-              table.insert(filtered_linters, name)
-            end
+      if names then
+        for _, name in ipairs(names) do
+          local linter = linters[name]
+          if not linter or (linter and linter.condition()) then
+            table.insert(filtered_linters, name)
           end
-        end
-
-        if #filtered_linters > 0 then
-          lint.try_lint(filtered_linters)
         end
       end
 
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = try_lint,
-      })
+      if #filtered_linters > 0 then
+        lint.try_lint(filtered_linters)
+      end
+    end
 
-      vim.keymap.set('n', '<leader>L', try_lint, { desc = 'Lint buffer' })
-    end,
-  },
+    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      group = lint_augroup,
+      callback = try_lint,
+    })
+
+    vim.keymap.set('n', '<leader>L', try_lint, { desc = 'Lint buffer' })
+  end,
 }
